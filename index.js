31f90d6eabad953233ebcab1e02190626917b60d
Connie Park <conniedc1206@gmail.com>
2022-05-23 10:53:38 -0700

    commit tree 2d2f2f0e61532641cc94ae96c98cd1f1c96ff761

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+//First, set up a DOMContentLoaded event listener to detect when the HTML page has loaded and the document is ready to be manipulated. 
+//Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+document.addEventListener('DOMContentLoaded', function(){
+    const p = document.querySelector('#text');
+    p.textContent = 'This is really cool!'
+});
+
